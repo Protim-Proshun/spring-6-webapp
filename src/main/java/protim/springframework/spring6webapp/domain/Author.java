57b07d4786d0a356988894e250ae14f307940d6f
Protim-Proshun <protim.proshun@gmail.com>
2024-04-27 07:41:33 -0400

    Initial commit

---
+++ src/main/java/protim/springframework/spring6webapp/domain/Author.java
@@ -0,0 +1,28 @@
+package protim.springframework.spring6webapp.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+}
